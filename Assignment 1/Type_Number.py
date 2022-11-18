dbd9e458ea3ec7cbae48095d6023a9bd3f601778
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-18 19:14:15 +0530

    Add files via upload

---
+++ Assignment 1/Type_Number.py
@@ -0,0 +1,10 @@
+# Set B 1.Write a python program to check if a number is +ve, -ve or zero..
+
+num = float(input("Enter a number: "))
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
+# @Code By Rj
