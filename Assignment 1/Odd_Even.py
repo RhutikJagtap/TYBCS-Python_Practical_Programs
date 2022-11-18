7413b3744cf168f93f9aae820e7d76f0b4bb17c7
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-18 19:20:09 +0530

    Add files via upload

---
+++ Assignment 1/Odd_Even.py
@@ -0,0 +1,10 @@
+# 2.Write a python program to check if a number is odd or even.
+
+# Take Input From User
+n = int(input("Enter Number:"))
+# Check Condition
+if (n % 2) == 0:
+    print(n, "is an even number")
+else:
+    print(n, "is odd number")
+#@Code By Rj 
