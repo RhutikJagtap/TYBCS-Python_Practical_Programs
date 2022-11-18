9e4d1fe366d343568bf872399448a18a20b48c71
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-18 19:05:48 +0530

    Update 2.Swap_No.py

--- Assignment 1/2.Swap_No.py
+++ Assignment 1/2.Swap_No.py
@@ -1,4 +1,4 @@
-# 2.Write Python program to Swap two Number
+#Set A 2.Write Python program to Swap two Number
 # Take inputs from the user
 x = int(input("Enter x : "))
 y = int(input("Enter y : "))
