4921fbfcd4a87215f70320de21dac65954212634
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-12-10 18:24:39 +0530

    Add files via upload

---
+++ Assignment 2/Sum_List.py
@@ -0,0 +1,18 @@
+# Set A-Functions
+# Write a Python function to sum all the numbers in a list.
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+
+
+print(sum((5, 2, 3, 4, 6)))
+
+# @Code By Rj
+
+"""
+Output:
+20
+"""
