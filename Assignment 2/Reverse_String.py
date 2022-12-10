4d88304b7711d23ff51e79cd5d34f37fff1cecb9
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-12-10 18:36:58 +0530

    Add files via upload

---
+++ Assignment 2/Reverse_String.py
@@ -0,0 +1,19 @@
+# Write a Python program to reverse a string.
+def reverse_string(str):
+    str1 = ""   # Declaring empty string to store the reversed string
+    for i in str:
+        str1 = i + str1
+    return str1
+
+
+string = input('Enter String : ')
+
+print("The original string is :", string)
+print("The reverse string is : ", reverse_string(string))
+
+# @Code By Rj
+"""
+The original string is :
+ hello python
+The reverse string is :  nohtyp olleh
+"""
