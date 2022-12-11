5b0e17abd98aaf34ff087064d30aa098a81337a8
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-12-11 18:14:15 +0530

    Create Append_Items_List.py

---
+++ Assignment 2/Practice Programs/Append_Items_List.py
@@ -0,0 +1,17 @@
+# 1. Write a Python program to append items from a specified list.
+
+from array import *
+num_list = [1, 2, 6, -8]
+array_num = array('i', [])
+print("Items in the list: " + str(num_list))
+print("Append items from the list: ")
+array_num.fromlist(num_list)
+print("Items in the array: "+str(array_num))
+
+# @Code By Rj
+'''
+Output:
+Items in the list: [1, 2, 6, -8]
+Append items from the list: 
+Items in the array: array('i', [1, 2, 6, -8])
+'''
